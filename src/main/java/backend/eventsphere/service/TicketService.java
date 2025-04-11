a7ce4f63de0107656d6a8c755ca51b065660a0f6
Anders35 <andersleo05@gmail.com>
2025-04-11 20:49:26 +0700

    [GREEN] implement TicketService

--- src/main/java/backend/eventsphere/service/TicketService.java
+++ src/main/java/backend/eventsphere/service/TicketService.java
@@ -8,15 +8,25 @@ import java.util.Map;
 import java.util.UUID;
 
 public class TicketService {
-    private final TicketRepository ticketRepository = new TicketRepository();
-    private final TicketFactory ticketFactory = new TicketFactory();
+    private final TicketRepository ticketRepository;
+    private final TicketFactory ticketFactory;
 
     public TicketService(TicketRepository ticketRepository, TicketFactory ticketFactory) {
+        this.ticketRepository = ticketRepository;
+        this.ticketFactory = ticketFactory;
     }
 
-    public Ticket createTicket(UUID eventId, String ticketType, Double price, Integer quota) {return null;}
+    public Ticket createTicket(UUID eventId, String ticketType, Double price, Integer quota) {
+        Ticket ticket = ticketFactory.createTicket(eventId, ticketType, price, quota);
+        ticketRepository.save(ticket);
+        return ticket;
+    }
 
-    public Map<UUID, Ticket> getTicketsByEvent(UUID eventId) {return null;}
+    public Map<UUID, Ticket> getTicketsByEvent(UUID eventId) {
+        return ticketRepository.listByEvent(eventId);
+    }
 
-    public boolean deleteTicket(UUID ticketId) {return false;}
+    public boolean deleteTicket(UUID ticketId) {
+        return ticketRepository.delete(ticketId);
+    }
 }
