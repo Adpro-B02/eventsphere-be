994163b98b15608c8a900a6fe77c588d74de1804
kaachiii <kaachi579@gmail.com>
2025-05-02 13:29:47 +0700

    setting database

--- src/main/java/backend/eventsphere/event/model/Event.java
+++ src/main/java/backend/eventsphere/event/model/Event.java
@@ -12,28 +12,16 @@ public class Event {
 
     private final UUID id;
     private final UUID organizerId;
+    private final String name;
+    private final Long ticketPrice;
+    private final LocalDateTime eventDateTime;
+    private final String location;
+    private final String description;
 
     @Setter
-    private String name;
-
-    @Setter
-    private Long ticketPrice;
-
-    @Setter
-    private LocalDateTime eventDateTime;
-
-    @Setter
-    private String location;
-
-    @Setter
-    private String description;
-
-    @Setter
-    private String link_image;
-
     private String status;
 
-    public Event(UUID organizerId, String name, Long ticketPrice, LocalDateTime eventDateTime, String location, String description, String link_image) {
+    public Event(UUID organizerId, String name, Long ticketPrice, LocalDateTime eventDateTime, String location, String description) {
         this.id = UUID.randomUUID();
         this.organizerId = organizerId;
         this.name = name;
@@ -41,17 +29,16 @@ public class Event {
         this.eventDateTime = eventDateTime;
         this.location = location;
         this.description = description;
-        this.link_image = link_image;
         this.status = "PLANNED"; // default status
     }
 
-    public Event(UUID organizerId, String name, Long ticketPrice, LocalDateTime eventDateTime, String location, String description, String status, String link_image) {
-        this(organizerId, name, ticketPrice, eventDateTime, location, description, link_image);
+    public Event(UUID organizerId, String name, Long ticketPrice, LocalDateTime eventDateTime, String location, String description, String status) {
+        this(organizerId, name, ticketPrice, eventDateTime, location, description);
         setStatus(status); // validasi dilakukan di setter
     }
 
     public void setStatus(String status) {
-        String[] allowedStatus = {"PLANNED", "CANCELLED", "COMPLETED"};
+        String[] allowedStatus = {"PLANNED", "CANCELLED", "COMPLETED", "POSTPONED"};
         if (Arrays.stream(allowedStatus).noneMatch(s -> s.equals(status))) {
             throw new IllegalArgumentException("Invalid event status: " + status);
         }
