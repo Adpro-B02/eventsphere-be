1da65a6395502cdb0777fb02d8f636a8254d0f5d
kaachiii <kaachi579@gmail.com>
2025-05-08 01:24:38 +0700

    [GREEN] Implement controller to add event

--- src/main/java/backend/eventsphere/event/controller/EventController.java
+++ src/main/java/backend/eventsphere/event/controller/EventController.java
@@ -5,9 +5,12 @@ import backend.eventsphere.event.service.EventService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/events")
@@ -24,5 +27,19 @@ public class EventController {
         List<Event> events = eventService.getAllEvents();
         model.addAttribute("events", events);
         return "event/events";
+    }
+
+    @GetMapping("/create")
+    public String createEventPage(Model model) {
+        Event event = new Event();
+        event.setOrganizerId(UUID.fromString("00000000-0000-0000-0000-000000000001")); // login user ID (organizer), sementara hardcoded
+        model.addAttribute("event", event);
+        return "event/createEvent";
+    }
+
+    @PostMapping("/create")
+    public String createEventPost(@ModelAttribute Event event) {
+        eventService.addEvent(event);
+        return "redirect:/events";
     }
 }
