7aaded8e5a7e065359d527caf721b9f864997116
kaachiii <kaachi579@gmail.com>
2025-05-07 23:25:19 +0700

    [REFACTOR] refactor controller event

--- src/main/java/backend/eventsphere/event/controller/EventController.java
+++ src/main/java/backend/eventsphere/event/controller/EventController.java
@@ -5,10 +5,12 @@ import backend.eventsphere.event.service.EventService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/events")
 public class EventController {
 
     private final EventService eventService;
@@ -17,7 +19,7 @@ public class EventController {
         this.eventService = eventService;
     }
 
-    @GetMapping("/events")
+    @GetMapping
     public String listEvents(Model model) {
         List<Event> events = eventService.getAllEvents();
         model.addAttribute("events", events);
