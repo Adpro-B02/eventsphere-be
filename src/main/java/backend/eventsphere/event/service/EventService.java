1448bbf42a2fef84497c4bfebe1d528266a8150a
kaachiii <kaachi579@gmail.com>
2025-05-07 22:35:32 +0700

    [GREEN] Implement EventService

---
+++ src/main/java/backend/eventsphere/event/service/EventService.java
@@ -0,0 +1,21 @@
+package backend.eventsphere.event.service;
+
+import backend.eventsphere.event.model.Event;
+import backend.eventsphere.event.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public EventService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+}
