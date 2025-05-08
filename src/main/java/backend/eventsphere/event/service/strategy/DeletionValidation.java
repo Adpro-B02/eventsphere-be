d3f3e4f391a724f646472084e2c7f50fa2810802
kaachiii <kaachi579@gmail.com>
2025-05-08 10:06:09 +0700

    [RED] Implement DeletionValidation

---
+++ src/main/java/backend/eventsphere/event/service/strategy/DeletionValidation.java
@@ -0,0 +1,15 @@
+package backend.eventsphere.event.service.strategy;
+
+import backend.eventsphere.event.model.Event;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeletionValidation implements ValidationStrategy {
+
+    @Override
+    public void validate(Event event) {
+        if (!(event.getStatus().equals("CANCELLED") || event.getStatus().equals("COMPLETED"))) {
+            throw new IllegalStateException("Event hanya bisa dihapus jika statusnya CANCELLED atau COMPLETED");
+        }
+    }
+}
