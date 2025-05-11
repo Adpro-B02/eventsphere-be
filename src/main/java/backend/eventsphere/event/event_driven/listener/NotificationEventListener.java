e0a1585263388e437bf54cef9d8277bd78944e45
kaachiii <kaachi579@gmail.com>
2025-05-12 02:18:00 +0700

    create listener when event deleted

--- src/main/java/backend/eventsphere/event/event_driven/listener/NotificationEventListener.java
+++ src/main/java/backend/eventsphere/event/event_driven/listener/NotificationEventListener.java
@@ -1,6 +1,7 @@
 package backend.eventsphere.event.event_driven.listener;
 
 import backend.eventsphere.event.event_driven.EventCreatedEvent;
+import backend.eventsphere.event.event_driven.EventDeletedEvent;
 import backend.eventsphere.event.event_driven.EventUpdatedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -16,5 +17,10 @@ public class NotificationEventListener {
     @EventListener
     public void handleEventUpdated(EventUpdatedEvent event) {
         System.out.println("Notifikasi: Event diupdate - " + event.getEvent().getName());
+    }
+
+    @EventListener
+    public void handleEventDeleted(EventDeletedEvent event) {
+        System.out.println("Notifikasi: Event dihapus - " + event.getEventId());
     }
 }
