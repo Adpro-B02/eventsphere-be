8c922beb3c89e0fe1b5b4ebeb53c1832c3759525
Dion <diodavis123@gmail.com>
2025-05-21 21:50:13 +0700

    Add Feature Auth

---
+++ src/main/java/backend/eventsphere/auth/dto/LoginDto.java
@@ -0,0 +1,13 @@
+package backend.eventsphere.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+    private String emailOrUsername;
+    private String password;
+}
