41311559620779491c0e6c0522368b87ed24814f
rizqyazzahra <rizqyazzahraa@gmail.com>
2025-05-26 05:25:04 +0700

    [GREEN] Promo fe

---
+++ src/main/java/backend/eventsphere/promo/controller/KodePromoViewController.java
@@ -0,0 +1,33 @@
+package backend.eventsphere.promo.controller;
+
+import backend.eventsphere.auth.model.User;
+import backend.eventsphere.auth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class KodePromoViewController {
+
+    private final UserService userService;
+
+    @GetMapping("/promos")
+    public String showPromosPage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User.UserRole role = null;
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            User user = userService.findByUsername(username);
+            if (user != null) {
+                role = user.getRole();
+            }
+        }
+        model.addAttribute("userRole", role);
+        return "promo/promo";
+    }
+
+}
